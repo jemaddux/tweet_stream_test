0018fc3783e809da3e4a3b00b4548f05c2eff54b
John Maddux <jemaddux@gmail.com>
2013-05-18 18:38:17 -0600

    FIRST

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweetStreamTest::Application.config.secret_token = '7b372e27d9acd1243b17de57ab5758219bf56f043777fb9f26d5d26ce5af0496100b41d3f45e22f7f2b88edcb2cfa3a6abb0c5718d5518d8b951618bb6474414'
